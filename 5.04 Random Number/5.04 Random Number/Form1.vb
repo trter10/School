16a1ba95741260867b03290cb749ee84645ab0d1
trter10 <trter20@gmail.com>
2014-10-23 10:21:24 -0400

    (º᷄ᴥº᷅)  ﻿
    (º᷄ᴥº᷅)  ﻿

---
+++ 5.04 Random Number/5.04 Random Number/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    'Luke Frazier
+    '5.04 Random Number
+    '10/23/2014
+
+    Private Sub btnGenerate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGenerate.Click
+        Dim rand As New System.Random
+        Dim decFirst As Decimal = rand.NextDouble
+        Dim intSecond As Integer = rand.Next
+        Dim intThird As Integer = rand.Next(1, 100)
+        Dim intFourth As Integer = rand.Next(11, 50)
+
+        lblOutput.Text = decFirst & vbCrLf & intSecond & vbCrLf & intThird & vbCrLf & intFourth
+    End Sub
+End Class
